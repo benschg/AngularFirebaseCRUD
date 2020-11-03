a0df22699b91804ea320e3f90f14c2fb4c8ea54b
Benjamin Grauer <grauerb@gmail.com>
2020-11-03 21:06:33 +0100

    Added Materialize

--- StaerkenSache/src/app/app.module.ts
+++ StaerkenSache/src/app/app.module.ts
@@ -1,16 +1,15 @@
+import { GreatExperiencesService } from './shared/great-experiences.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { GreatExperienceComponent } from './great-experience/great-experience.component';
 import { GreatExperiencesComponent } from './great-experiences/great-experiences.component';
 import { GreatExperienceListComponent } from './great-experience-list/great-experience-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GreatExperienceComponent,
     GreatExperiencesComponent,
     GreatExperienceListComponent
   ],
@@ -18,7 +17,9 @@ import { GreatExperienceListComponent } from './great-experience-list/great-expe
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    GreatExperiencesService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
