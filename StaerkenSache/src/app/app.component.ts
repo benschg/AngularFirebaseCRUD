7d154dede5100462bb913440bfb89715ed4db1bc
Benjamin Grauer <grauerb@gmail.com>
2020-11-03 23:13:38 +0100

    Added Email Verification

--- StaerkenSache/src/app/app.component.ts
+++ StaerkenSache/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from './service/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'StaerkenSache';
+
+  email: string;
+  password: string;
+
+  constructor(public authService: AuthService) {
+}
+
+
+  // tslint:disable-next-line: typedef
+  signup() {
+  this.authService.signup(this.email, this.password);
+  this.email = this.password = '';
+}
+  // tslint:disable-next-line: typedef
+  login() {
+  this.authService.login(this.email, this.password);
+  this.email = this.password = '';
+}
+
+  // tslint:disable-next-line: typedef
+  logout() {
+  this.authService.logout();
+}
 }
--- StaerkenSache/src/app/service/auth.service.ts
+++ StaerkenSache/src/app/service/auth.service.ts
@@ -2,12 +2,13 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { Observable } from 'rxjs';
+import firebase from 'firebase/app';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  user: Observable<firebase.User>;
+  user: Observable<firebase.User | null>;
 
   constructor(private firebaseAuth: AngularFireAuth) {
     this.user = firebaseAuth.authState;
@@ -17,10 +18,14 @@ export class AuthService {
     this.firebaseAuth
       .createUserWithEmailAndPassword(email, password)
       .then(value => {
+         value.user.sendEmailVerification();
+      })
+
+      .then(value => {
         console.log('Success!', value);
       })
       .catch(err => {
-        console.log('Something went wrong:',err.message);
+        console.log('Something went wrong:', err.message);
       });
   }
 
@@ -28,7 +33,12 @@ export class AuthService {
     this.firebaseAuth
       .signInWithEmailAndPassword(email, password)
       .then(value => {
+        if (value.user.emailVerified) {
         console.log('Nice, it worked!');
+        }
+        else {
+          throw new Error('Email is not verified yet.');
+        }
       })
       .catch(err => {
         console.log('Something went wrong:',err.message);
